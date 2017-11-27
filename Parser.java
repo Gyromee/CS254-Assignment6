b58f019ec1a7575464332473254c9a8ba6aa0b6d
Mevin Chen <mevin.chen@csu.fullerton.edu>
2017-11-26 21:17:16 -0800

    Added D to S commands for each case

--- Parser.java
+++ Parser.java
@@ -1,3 +1,5 @@
+package assignment.project;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -40,7 +42,7 @@ public class Parser{
                 data = splitLine[7];
                 numOfWords = Integer.parseInt(data,16) / 2;
                 //Output for the command
-                System.out.println("Line " + lineNumber + ": " + cycle + " "
+                System.out.println("\nLine " + lineNumber + ": " + cycle + " "
                                     + command + " comand: " + numOfWords + " words.");
                 //Parse the data of the S-to-D command
                 int wordNum = 0;
@@ -94,10 +96,10 @@ public class Parser{
                                             if (k == 0) {
                                                 System.out.print("Line " + lineNumber + ": Word 0: " );                                         
                                                 System.out.print("Rec_ctrl = " + Integer.parseInt(bin.substring(1,3)));
-                                                if (bin.matches("^.00[01]+")) System.out.println("(no recording)");
-                                                else if (bin.matches("^.10[01]+")) System.out.println("(no processing)");
-                                                else if (bin.matches("^.11[01]+")) System.out.println("(processing and recording)");
-                                                else System.out.println("(unknown)");
+                                                if (bin.matches("^.00[01]+")) System.out.println(" (no recording)");
+                                                else if (bin.matches("^.10[01]+")) System.out.println(" (no processing)");
+                                                else if (bin.matches("^.11[01]+")) System.out.println(" (processing and recording)");
+                                                else System.out.println(" (unknown)");
                                             }
                                             //Word 1
                                             if (k == 1) {
@@ -106,7 +108,7 @@ public class Parser{
                                                 if (bin.matches("^100[01]+")) System.out.println("type A");
                                                 else if (bin.matches("^101[01]+")) System.out.println("type B");
                                                 else if (bin.matches("^110[01]+")) System.out.println("type C");
-                                                else System.out.println("(unknown)");
+                                                else System.out.println(" (unknown)");
                                             }
                                         break;
                                     case "40000820":
@@ -114,9 +116,9 @@ public class Parser{
                                         if (k == 0) {
                                             System.out.print("Line " + lineNumber + ": Word 4: " );                                         
                                             System.out.print("Rec_Raw = " + Integer.parseInt(bin.substring(15,16)));
-                                            if (bin.matches("^.{15}0")) System.out.println("(disable)");
-                                            else if (bin.matches("^.{15}1")) System.out.println("(enable)");
-                                            else System.out.println("(unknown)");
+                                            if (bin.matches("^.{15}0")) System.out.println(" (disable)");
+                                            else if (bin.matches("^.{15}1")) System.out.println(" (enable)");
+                                            else System.out.println(" (unknown)");
                                         }
                                         //Word 5
                                         if (k == 1) {
@@ -157,18 +159,187 @@ public class Parser{
                 data = splitLine[7];
                 numOfWords = Integer.parseInt(data,16) / 2;
                 //Output for the command
-                System.out.println("Line " + lineNumber + ": " + cycle + " "
+                System.out.println("\nLine " + lineNumber + ": " + cycle + " "
                                     + command + " comand: " + numOfWords + " words.");
                 
-                //Parse the data for D-to-S command
-                /* <Conditional statements similar to the ones
-                 * in the S-to-D command parsing> goes here 
-                 */                 
+                /* *****************************************************
+                *********Parse the data for D-to-S command*************
+                ***************************************************** */
+                
+                int wordNum = 0;
+                boolean isReverse = false;
+                boolean checkedFirstAddress = false;
+                int k = 0;
+                
+                while (wordNum < numOfWords) {  
+                    line = br.readLine();
+                    lineNumber++;
+                    splitLine = line.trim().split("\\s+");
+                    address = splitLine[6]; 
+                    data = splitLine[7];
+       
+                    if (checkedFirstAddress == false) {
+                        if (Integer.parseInt(address, 16) == ((Integer.parseInt("40000C20", 16)) + numOfWords*2 - 4)) {
+                            isReverse = true;
+                        }
+                        checkedFirstAddress = true;
+                    }
+                    
+                    if(isReverse == true)
+                        k = 1;
+                    else
+                        k = 0;
+                    //Check if our address is between 0x40000818 to 0x40000C14 
+                    if((Integer.parseInt("40000C20", 16)) <= (Integer.parseInt(address, 16))
+                            &&  (Integer.parseInt(address, 16)) < ((Integer.parseInt("4000101C", 16)) + numOfWords*2)) {
+                            ArrayList<String> wordsAsArray = splitWords(data, 4);
+                            for (int i = 0; i < wordsAsArray.size(); i++) {
+                                
+                                /* <Conditional statements that parse
+                                 * each word using the word fields
+                                 * chart> goes here 
+                                 */
+                                
+                                //Split our split word into each split. Such as 1093 is 1,0,9,3 in an array
+                                ArrayList<String> splitWord = splitWords(wordsAsArray.get(k), 1);
+                                String bin = "";
+                                //Convert hex to binary, and formats it with 0's
+                                for (String x: splitWord)
+                                {
+                                    int hex = Integer.parseInt(x, 16);
+                                    bin += String.format("%4s", Integer.toBinaryString(hex)).replace(' ', '0');
+                                    
+                                }
+    
+                                switch (address) {
+                                    case "40000C20": 
+                                            //Word 0
+                                            if (k == 0) {
+                                                System.out.print("Line " + lineNumber + ": Word 0: " );                                         
+                                                System.out.print("Rec_ctrl = " + Integer.parseInt(bin.substring(1,3)));
+                                                if (bin.matches("^.00[01]+")) System.out.println("(no recording)");
+                                                else if (bin.matches("^.10[01]+")) System.out.println(" (no processing)");
+                                                else if (bin.matches("^.11[01]+")) System.out.println(" (processing and recording)");
+                                                else System.out.println(" (unknown)");
+                                            }
+                                            //Word 1
+                                            if (k == 1) {
+                                                System.out.print("Line " + lineNumber + ": Word 1: " );                                         
+                                                System.out.print("Cmd_Type = " + Integer.parseInt(bin.substring(0,3)));
+                                                if (bin.matches("^100[01]+")) System.out.println("type A");
+                                                else if (bin.matches("^101[01]+")) System.out.println("type B");
+                                                else if (bin.matches("^110[01]+")) System.out.println("type C");
+                                                else System.out.println(" (unknown)");
+                                            }
+                                        break;
+                                    case "40000C28":
+                                        //Word 4
+                                        if (k == 0) {
+                                            System.out.print("Line " + lineNumber + ": Word 4: " );                                         
+                                            System.out.print("Rec_Raw = " + Integer.parseInt(bin.substring(15,16)));
+                                            if (bin.matches("^.{15}0")) System.out.println(" (disable)");
+                                            else if (bin.matches("^.{15}1")) System.out.println(" (enable)");
+                                            else System.out.println(" (unknown)");
+                                        }
+                                        //Word 5
+                                        if (k == 1) {
+                                            System.out.print("Line " + lineNumber + ": Word 5: " );                                         
+                                            System.out.println("Cmd_id = " +(Integer.parseInt(bin.substring(9,16), 2)));
+                                        }
+                                        break;
+                                    case "40000C34":
+                                        //Word 10
+                                        if (k == 0) {
+                                            System.out.print("Line " + lineNumber + ": Word 10: " );                                            
+                                            System.out.println("Num_Responses = " + Integer.parseInt(bin.substring(0,5), 2));
+                                        }
+                                        break;
+                                    case "40000C3C":
+                                    	//Word 15
+                                    	if (k == 0)
+                                    	{
+                                    		 System.out.print("Line " + lineNumber + ": Word 15: " );  
+                                    		 System.out.print("Reset_Enable = " + Integer.parseInt(bin.substring(13,14)));
+                                    		 if(bin.matches("^[01]{13}1[01][01]")) System.out.println(" (Enable)");
+                                    		 else if (bin.matches("^[01]{13}0[01][01]")) System.out.println(" (Disable)");
+                                    		 else System.out.println("(unknown)");
+                                    	}
+                                    	break;
+                                    case "40000C4C":
+                                    	//Word 22
+                                    	if (k == 0)
+                                    	{
+                                    		 System.out.print("Line " + lineNumber + ": Word 22: " );  
+                                    		 System.out.print("Direction = " + Integer.parseInt(bin.substring(12,13)));
+                                    		 if(bin.matches("^[01]{12}1[01]{3}")) System.out.println(" (left)");
+                                    		 else if (bin.matches("^[01]{12}0[01]{3}")) System.out.println(" (right)");
+                                    		 else System.out.println("(unknown)");
+                                    	}
+                                    	break;
+                                    case "40000C60":
+                                    	//Word 32
+                                    	if (k == 0)
+                                    	{
+                                    		 System.out.print("Line " + lineNumber + ": Word 32: " );  
+                                    		 System.out.println("Num_Samples = " + (Integer.parseInt(bin.substring(1,16), 2)));
+                                    	}
+                                    	break;
+                                    case "40000C68":
+                                    	//Word 37
+                                    	if(k == 0)
+                                    	{
+                                    		System.out.print("Line " + lineNumber + ": Word 37: " );  
+                                    		System.out.print("Parity = " + Integer.parseInt(bin.substring(0,1)));
+                                    		if (bin.matches("^0[01]{15}")) System.out.println(" (even)");
+                                    		else if (bin.matches("^1[01]{15}")) System.out.println(" (odd)");
+                                    		else System.out.println("(unknown)");
+                                    	}
+                                    	break;
+                                    case "40000C6C":
+                                    	//Word 38
+                                    	if (k == 0)
+                                    	{
+                                    		System.out.print("Line " + lineNumber + ": Word 38: " ); 
+                                    		System.out.print("Test = " + Integer.parseInt(bin.substring(1,2)));
+                                    		if (bin.matches("^[01]0[01]{14}")) System.out.println(" (disable)");
+                                    		else if (bin.matches("^[01]1[01]{14}")) System.out.println(" (enable)");
+                                    		else System.out.println("(unknown)");
+                                    	}
+                                    	break;
+                                    case "40000C70":
+                                    	//Word 40
+                                    	if (k == 0)
+                                    	{
+                                    		System.out.print("Line " + lineNumber + ": Word 40: " ); 
+                                    		System.out.print("Test = " + Integer.parseInt(bin.substring(8,9)));
+                                    		if (bin.matches("^[01]{8}0[01]{7}")) System.out.println(" (disable)");
+                                    		else if (bin.matches("^[01]{8}1[01]{7}")) System.out.println(" (enable)");
+                                    		else System.out.println(" (unknown)");
+                                    	}
+                                    	if (k == 1)
+                                    	{
+                                    		System.out.print("Line " + lineNumber + ": Word 41: " );
+                                    		System.out.println("Code = " +(Integer.parseInt(bin.substring(1,8), 2)));
+                                    	}
+                                    	break;
+                                    default: 
+                                        break;
+                                }
+                                
+                                wordNum++;
+                                //System.out.println("k: " + k + ",i: " + i + ",bin:" + bin);
+                                if (isReverse == true)
+                                    k--;
+                                else
+                                    k++;
+                        }
+                    }   
+                }
             }
             
             //This line is only here so your console isn't thousands of
             //lines long while testing
-             if (lineNumber > 800) break reading;
+             //if (lineNumber > 800) break reading;
         }
        }catch (Exception e){
          e.printStackTrace();
@@ -186,3 +357,7 @@ public class Parser{
         return splitString;
     }
 }
+
+
+
+
