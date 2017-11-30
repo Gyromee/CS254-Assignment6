43c04da03478288081d892a94169bf41422561c2
Gyromee <Gyromee@gmail.com>
2017-11-29 22:31:31 -0800

    now prompts user for filename

--- Driver.java
+++ Driver.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class Driver {
 	public static void main(String[] args) {
-		Parser p1 = new Parser("test_data.log");
+		System.out.println("Please enter file name");
+		Scanner s = new Scanner(System.in);
+		String filename = s.nextLine();
+		Parser p1 = new Parser(filename);
 		p1.parse();
+		
+		s.close();
+		
 	}
 }
