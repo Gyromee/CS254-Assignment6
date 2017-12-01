f1c7a16d9d1df59fb8ccf2666a7ebb8bb7bb56f1
Mevin Chen <mevin.chen@csu.fullerotn.edu>
2017-11-30 16:45:32 -0800

    Cleaned up lines we didn't need.

--- Driver.java
+++ Driver.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
 public class Driver {
 	public static void main(String[] args) {
 		String filename;
 		filename = args[0];
 		Parser p1 = new Parser(filename);
 		p1.parse();
-		
-		
 	}
 }
